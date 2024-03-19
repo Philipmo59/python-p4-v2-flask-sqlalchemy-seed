8a6c7358215b2082606f1535f616361c467bf3b0
philipmo <philipmo59@gmail.com>
2024-03-19 09:40:14 -0400

    finished

--- server/seed.py
+++ server/seed.py
@@ -3,17 +3,28 @@
 
 from app import app
 from models import db, Pet
+from faker import Faker
+from random import choice as rc
 
 with app.app_context():
 
+    #Instantilize the faker generator
+
+    faker = Faker()
+
+    #Deletes all rows in the "pets" table
+    Pet.query.delete()
     # Create an empty list
     pets = []
 
     # Add some Pet instances to the list
-    pets.append(Pet(name = "Fido", species = "Dog"))
-    pets.append(Pet(name = "Whiskers", species = "Cat"))
-    pets.append(Pet(name = "Hermie", species = "Hamster"))
+    species = ['Dog','Cat','Chicken','Hamster','Turtle']
+    
+    #Add some pet instances
 
+    for n in range(10):
+        pet = Pet(name=faker.first_name(), species=rc(species))
+        pets.append(pet)
     # Insert each Pet in the list into the database table
     db.session.add_all(pets)
 
